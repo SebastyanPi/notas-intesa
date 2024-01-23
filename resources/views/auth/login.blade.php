a86a8760308b5015066f13561fc5c386463cf092
SebastyanDev <sepineda@unicesar.edu.co>
2024-01-23 16:04:06 -0500

    Segundo Cambio login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -176,7 +176,7 @@
         </div>
         </div>
         <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('http://127.0.0.1:8000/img/intesae1.jfif');
+        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('{{  $URL }}img/intesae1.jfif');
                       background-size: cover;">
         <span class="mask bg-gradient-primary opacity-1"></span>
         <h4 class="mt-5 text-white font-weight-bolder position-relative">"Egresada"</h4>

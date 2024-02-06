347e5c9e32e161b51c76b401420adde1f5c0ab43
SebastyanDev <sepineda@unicesar.edu.co>
2024-02-06 15:21:37 -0500

    Cambios en rutas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,14 +16,14 @@
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
-    <link href="@php echo $URL."assets/css/nucleo-icons.css" @endphp" rel="stylesheet" />
-    <link href="@php echo $URL."assets/css/nucleo-svg.css" @endphp " rel="stylesheet" />
+    <link href="{{ env('APP_URL') }}assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="{{ env('APP_URL') }}assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href="{{ env('APP_URL') }}assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- CSS Files -->
-    <link id="pagestyle" href="@php echo $URL."assets/css/argon-dashboard.css" @endphp" rel="stylesheet" />
-    <link id="pagestyle" href="@php echo $URL."assets/css/app.css" @endphp" rel="stylesheet" />
+    <link id="pagestyle" href="{{ env('APP_URL') }}assets/css/argon-dashboard.css" rel="stylesheet" />
+    <link id="pagestyle" href="{{ env('APP_URL') }}assets/css/app.css" rel="stylesheet" />
     @notifyCss
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     @livewireStyles
@@ -59,10 +59,10 @@
     <x-notify::notify />        
     @notifyJs
     <!--   Core JS Files   -->
-    <script src="@php echo $URL."assets/js/core/popper.min.js"@endphp"></script>
-    <script src="@php echo $URL."assets/js/core/bootstrap.min.js"@endphp"></script>
-    <script src="@php echo $URL."assets/js/plugins/perfect-scrollbar.min.js"@endphp"></script>
-    <script src="@php echo $URL."assets/js/plugins/smooth-scrollbar.min.js"@endphp"></script>
+    <script src="{{ env('APP_URL') }}assets/js/core/popper.min.js"></script>
+    <script src="{{ env('APP_URL') }}assets/js/core/bootstrap.min.js"></script>
+    <script src="{{ env('APP_URL') }}assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="{{ env('APP_URL') }}assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -75,7 +75,7 @@
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="assets/js/argon-dashboard.js"></script>
+    <script src="{{ env('APP_URL') }}assets/js/argon-dashboard.js"></script>
     @stack('js');
 </body>
 

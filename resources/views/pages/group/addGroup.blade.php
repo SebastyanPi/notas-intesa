98f7bc4deb6674aea25fcf6373e621f7170c3095
SebastyanDev <sepineda@unicesar.edu.co>
2024-02-08 11:13:12 -0500

    Modal

--- resources/views/pages/group/addGroup.blade.php
+++ resources/views/pages/group/addGroup.blade.php
@@ -115,7 +115,9 @@
                 </div>
                 
             </div>
-         
+            <button button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
           
         </div>
         <div class="modal-body">
